d966f73b7dbd8b00881f0e4f1a317f2ec5250ea6
swcsend <swcsend@gmail.com>
2017-05-21 00:03:27 +0800

    Swithed MySQL

--- blog/application.py
+++ blog/application.py
@@ -8,6 +8,13 @@ from blog.db import *
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(bp_views)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@127.0.0.1:3306/blog'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    with app.app_context():
+        # Extensions like Flask-SQLAlchemy now know what the "current" app
+        # is while within this block. Therefore, you can now run........
+        db.create_all()
 
     return app
 
@@ -18,7 +25,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 if __name__ == '__main__':
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
     app.run(host='127.0.0.1', port=5000)
