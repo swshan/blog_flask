87762df196bb6a6dd585f3afafad45a404501b55
swcsend <swcsend@gmail.com>
2017-05-30 14:40:34 +0800

    db small change

--- blog/db.py
+++ blog/db.py
@@ -6,11 +6,13 @@ from sqlalchemy import Table, Column, MetaData
 from sqlalchemy import BigInteger, DateTime, String, CHAR, Text, Integer,ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session ,sessionmaker, relationship, backref
+from sqlalchemy.pool import QueuePool
+
 
 BaseModel = declarative_base()
 #session_engine = create_engine('sqlite://dbuser:password@localhost:5432/mydb')
 
-session_engine = create_engine('postgresql://dbuser:password@localhost:5432/mydb',  client_encoding='utf8',pool_recycle = 65)
+session_engine = create_engine('postgresql+psycopg2://dbuser:password@localhost:5432/mydb',  client_encoding='utf8',poolclass=QueuePool )
 Session = scoped_session(sessionmaker(session_engine))
 session = Session()
 
