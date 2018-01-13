dea2ccd242e0652885d9bc1077f46a78f059c557
abc <abc@abcde-Air.lan>
2018-01-13 21:40:11 +0800

    rework with peewee orm.

--- blog/models/db.py
+++ blog/models/db.py
@@ -1,68 +1,37 @@
 # coding=utf-8
-import datetime
 
-from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, MetaData
-from sqlalchemy import BigInteger, DateTime, String, CHAR, Text, Integer,ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session ,sessionmaker, relationship, backref
-from sqlalchemy.pool import QueuePool
+from peewee import *
 
 
-BaseModel = declarative_base()
-#session_engine = create_engine('sqlite://dbuser:password@localhost:5432/mydb')
+psql_db = PostgresqlDatabase('testdb', user='dbuser')
 
-session_engine = create_engine('postgresql+psycopg2://dbuser:password@localhost:5432/mydb',  client_encoding='utf8',poolclass=QueuePool )
-Session = scoped_session(sessionmaker(session_engine))
-## session = Session()
+class User(Model):
+    user_id = IntegerField()
+    user_name = CharField()
+    password = CharField()
+    email = CharField()
 
-class User(BaseModel):
-    __tablename__ = 'user'
+    class Meta:
+        database = psql_db
 
-    user_id = Column(String(64), primary_key=True)
-    name = Column(String(32), server_default='', nullable=False)
-    password = Column(String(32), server_default='', nullable=False)
-    email = Column(String(32), server_default='', nullable=False)
+class Post(Model):
+    post_id = IntegerField()
+    post_user_id = IntegerField()
+    post_category_id = IntegerField()
+    title = CharField()
+    post_body = TextField()
+    post_pubdate = DateTimeField()
+    is_display = BooleanField()
+	
+    class Meta:
+        database = psql_db
 
-class UserSession(BaseModel):
-    __tablename__ = 'usersession'
+class Category(Model):
+    category_id = IntegerField()
+    category_name = CharField()
 
-    session_id = Column(String(32), primary_key=True)
-    user_id = Column(BigInteger, index=True, nullable=False)
-    create_time = Column(DateTime)
-    expire_time = Column(DateTime)
-
-
-class Post(BaseModel):
-    __tablename__ = 'post'
-    
-    post_id = Column(Integer, primary_key=True,autoincrement=True)
-    title = Column(String(128))
-    body = Column(String(2048))
-    #category_id = Column()
-    category = Column(Text())
-    pub_date = Column(DateTime())
-
-    def __init__(self, title, body, category, pub_date):
-        self.title = title
-        self.body = body
-        self.category = category
-        if self.pub_date is None:
-            self.pub_date = datetime.date.today()
-        self.pub_date = datetime.date.today()
-
-    def to_json(self):
-        return { c.name: getattr(self, c.name) for c in self.__table__.columns }
+    class Meta:
+        database = psql_db
 
 
-class Categories(BaseModel):
-    __tablename__ = 'category'
-
-    category_id = Column(Integer, primary_key=True)
-    category = Column(String(128))
-    
-
-
-if __name__ == '__main__':
-	BaseModel.metadata.create_all(session_engine)
-
+psql_db.create_tables([User, Post, Category])
